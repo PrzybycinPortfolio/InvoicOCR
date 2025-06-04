3f8c7028056765e0ffabcf592542bca69a0691db
Karol Przybycin <przybycinkarol@gmail.com>
2025-06-04 22:09:06 +0200

    Converting to gray scale, gaussian blur

--- Program/Image.py
+++ Program/Image.py
@@ -11,12 +11,26 @@ class Image:
 
     def __init__(self,filename):
         self.filename = filename
-        self.orginal_img = cv2.imread(filename,cv2.IMREAD_COLOR)
+        self.img = cv2.imread(filename, cv2.IMREAD_COLOR)
 
     #show colorful input image
-    def show_original_image_window(self):
-        cv2.imshow("Orginal colorful input image", self.orginal_img)
+    def show_image_window(self):
+        cv2.imshow("Image", self.img)
         Image.do_not_close()
+
+    def convert_to_gray_scale(self):
+        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        self.img = gray
+
+    def gaussian_blur(self,level = 5):
+        if((level % 2) == 0):
+            level = level + 1
+        current_img = cv2.GaussianBlur(self.img, (level, level), 0)
+        self.img = current_img
+
+
+
+
 
 
 
--- Program/Main.py
+++ Program/Main.py
@@ -1,5 +1,12 @@
 import Image
 
+test = Image.Image('./original_images/test_colorful.png')
+test_level = 3
+test.gaussian_blur(test_level)
+test.convert_to_gray_scale()
+test.show_image_window()
+
+
 
 
 
